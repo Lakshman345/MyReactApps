78fc9b22cee2df6c1eab9a41ac6148a655b7ea55
Lakshmanarao Nidamanuri <Lakshmanarao.Nidamanuri@aexp.com>
2019-06-27 22:57:18 +0530

    deleted files

--- src/navroutes/ContactsComp.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-class ContactComp extends React.Component {
-  render() {
-    return <h1>Contact component</h1>;
-  }
-}
-export default ContactComp;
--- src/navroutes/deposit/Account.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-class Account extends React.Component {
-  render() {
-    return <h1>Account component</h1>;
-  }
-}
-export default Account;
